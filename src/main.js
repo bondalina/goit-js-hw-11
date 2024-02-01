03b9562a5bbe5b7030de2aa9f2573238510ff8f7
bondalina <142501520+bondalina@users.noreply.github.com>
2024-02-01 22:03:57 +0200

    task

--- src/main.js
+++ src/main.js
@@ -7,3 +7,63 @@ import "izitoast/dist/css/iziToast.min.css";
 import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+const searchForm = document.querySelector('.search-form')
+const searchInput = document.querySelector('.search-input')
+const galleryList = document.querySelector('.gallery-list')
+
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const inputValue = searchInput.value.trim();
+
+    if (inputValue === '') {
+        iziToast.error({
+            title: 'Error',
+            message: 'Please enter a search query!',
+            position: 'topRight',
+        });
+        return;
+    }
+
+    galleryList.innerHTML = '';
+
+    const apiKey = '36996517-56800863ae540be6945d0f4f2';
+    const url = `https://pixabay.com/api/?key=${apiKey}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.hits.length === 0) {
+                iziToast.error({
+                    message: 'Sorry, there are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                    timeout: 3000,
+                });
+                return;
+            }
+            
+            galleryList.innerHTML = data.hits.map(image => {
+                const imageLink = `
+                    <a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags} | Likes: ${image.likes} | Views: ${image.views} | Comments: ${image.comments} | Downloads: ${image.downloads}">
+                        <img src="${image.webformatURL}" alt="${image.tags}">
+                    </a>`;
+                
+                const listItem = `<li class="gallery-item">${imageLink}</li>`;
+
+                return listItem;
+            }).join('');
+
+            const lightbox = new SimpleLightbox('.gallery a');
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+});
+
+
