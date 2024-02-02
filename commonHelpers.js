0c39e4fc724de94bd1ef09af4a4238ce76bc3e8e
bondalina <142501520+bondalina@users.noreply.github.com>
2024-02-02 16:53:02 +0000

    Deploying to gh-pages from @ bondalina/goit-js-hw-11@983c52d668e64d289698c1536d7af085c909a3b9 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,14 +1,12 @@
-import{i as l,S as c}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const i of t.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=n(e);fetch(e.href,t)}})();const u=document.querySelector(".search-form"),m=document.querySelector(".search-input"),a=document.querySelector(".gallery-list");u.addEventListener("submit",s=>{s.preventDefault();const r=m.value.trim();if(r===""){l.error({title:"Error",message:"Please enter a search query!",position:"topRight"});return}a.innerHTML="";const o=`https://pixabay.com/api/?key=36996517-56800863ae540be6945d0f4f2&q=${r}&image_type=photo&orientation=horizontal&safesearch=true`;fetch(o).then(e=>{if(!e.ok)throw new Error("Network response was not ok");return e.json()}).then(e=>{if(e.hits.length===0){l.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:3e3});return}a.innerHTML=e.hits.map(t=>`
-        
-          <li class="gallery-item">
-            <a href="${t.largeImageURL}" data-lightbox="gallery" data-title="${t.tags}">
-                <img class="img-item" src="${t.webformatURL}" alt="${t.tags}">
-            </a>
-            <ul class="image-properties">
-                <li>Likes <br/>${t.likes}</li>
-                <li>Views <br/>${t.views}</li>
-                <li>Comments <br/>${t.comments}</li>
-                <li>Downloads <br/>${t.downloads}</li>
-            </ul>
-          </li>`).join(""),new c(".gallery a")}).catch(e=>{console.error("There has been a problem with your fetch operation:",e)})});
+import{i as a,S as m}from"./assets/vendor-46aac873.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const r of t.addedNodes)r.tagName==="LINK"&&r.rel==="modulepreload"&&i(r)}).observe(document,{childList:!0,subtree:!0});function l(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=l(e);fetch(e.href,t)}})();const f=document.querySelector(".search-form"),c=document.querySelector(".search-input"),u=document.querySelector(".gallery-list"),s=document.querySelector(".loader");f.addEventListener("submit",n=>{n.preventDefault();const o=c.value.trim();if(o===""){a.error({title:"Error",message:"Please enter a search query!",position:"topRight"});return}s.style.display="block",u.innerHTML="";const i=`https://pixabay.com/api/?key=36996517-56800863ae540be6945d0f4f2&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`;fetch(i).then(e=>{if(!e.ok)throw new Error("Network response was not ok");return e.json()}).then(e=>{if(e.hits.length===0){a.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",timeout:3e3});return}setTimeout(()=>{u.innerHTML=e.hits.map(r=>`<li class="gallery-item">
+                <a href="${r.largeImageURL}" data-lightbox="gallery" data-title="${r.tags}">
+                <img class="img-item" src="${r.webformatURL}" alt="${r.tags}">
+                </a>
+                <ul class="image-properties">
+                <li>Likes <br/>${r.likes}</li>
+                <li>Views <br/>${r.views}</li>
+                <li>Comments <br/>${r.comments}</li>
+                <li>Downloads <br/>${r.downloads}</li>
+                </ul>
+                </li>`).join(""),c.value="",s.style.display="none",new m(".gallery a").refresh()},2e3)}).catch(e=>{console.error("There has been a problem with your fetch operation:",e),s.style.display="none"})});
 //# sourceMappingURL=commonHelpers.js.map
